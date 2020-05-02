f4f1d754b2536cfae03fb663543ab89bb69d9ddb
1nd3e <vlad@hakkie.ru>
2020-05-02 17:51:18 +0300

    Почистил проект и организовал структуру файлов

---
+++ Bubbless/Scenes/GameScene.swift
@@ -0,0 +1,12 @@
+//
+//  GameScene.swift
+//  Bubbless
+//
+//  Created by Vladislav Kulikov on 02.05.2020.
+//  Copyright © 2020 Vladislav Kulikov. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {}
