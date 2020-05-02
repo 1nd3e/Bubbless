009be52042e17cf5f90f4fc3b11d9daf38f13b76
1nd3e <vlad@hakkie.ru>
2020-05-02 17:59:47 +0300

    Добавил сцене физические тела

--- Bubbless/Scenes/GameScene.swift
+++ Bubbless/Scenes/GameScene.swift
@@ -9,4 +9,28 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {}
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        addPhysicsEdges()
+    }
+    
+}
+
+// MARK: - Scene
+
+extension GameScene {
+    
+    private func addPhysicsEdges() {
+        let bodyLT = SKPhysicsBody(edgeFrom: CGPoint(x: frame.minX, y: frame.maxY), to: CGPoint(x: frame.minX, y: frame.minY))
+        let bodyRT = SKPhysicsBody(edgeFrom: CGPoint(x: frame.maxX, y: frame.maxY), to: CGPoint(x: frame.maxX, y: frame.minY))
+        let bodyBT = SKPhysicsBody(edgeFrom: CGPoint(x: frame.minX, y: frame.minY), to: CGPoint(x: frame.maxX, y: frame.minY))
+        
+        let physicsBody = SKPhysicsBody(bodies: [bodyLT, bodyRT, bodyBT])
+        physicsBody.isDynamic = false
+        physicsBody.affectedByGravity = false
+        
+        self.physicsBody = physicsBody
+    }
+    
+}
