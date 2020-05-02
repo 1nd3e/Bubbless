da89ec70d0df81a1f9d78a9c86340ee1f32c1158
1nd3e <vlad@hakkie.ru>
2020-05-02 21:22:27 +0300

    Написал логику генерации Bubble

--- Bubbless/Nodes/Entities/Bubble.swift
+++ Bubbless/Nodes/Entities/Bubble.swift
@@ -13,11 +13,13 @@ class Bubble: GKEntity {
     
     // MARK: - Properties
     
+    let size: CGSize
     let color: SKColor
     
     // MARK: - Initializers
     
     init(size: CGSize, color: SKColor) {
+        self.size = size
         self.color = color
         
         super.init()
@@ -34,6 +36,18 @@ class Bubble: GKEntity {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        let bubble = Bubble(size: size, color: color)
+        
+        if let rootNode = component(ofType: NodeComponent.self)?.node {
+            if let node = bubble.component(ofType: NodeComponent.self)?.node {
+                node.position = rootNode.position
+            }
+        }
+        
+        return bubble
     }
     
 }
--- Bubbless/Scenes/GameScene.swift
+++ Bubbless/Scenes/GameScene.swift
@@ -11,10 +11,19 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    private var bubbles = [Bubble]()
+    
     private var entities = Set<GKEntity>()
     
     override func didMove(to view: SKView) {
+        // Настраиваем параметры сцены игры
         addPhysicsEdges()
+        
+        // Подготавливаем элементы игры к работе
+        loadBubbles()
+        
+        // Запускаем игровую логику
+        spawnBubbles()
     }
     
 }
@@ -33,6 +42,73 @@ extension GameScene {
         physicsBody.affectedByGravity = false
         
         self.physicsBody = physicsBody
+    }
+    
+}
+
+// MARK: - Bubble Entities
+
+extension GameScene {
+    
+    private func loadBubble(with color: SKColor, size: CGSize, at position: CGPoint) -> Bubble {
+        let bubble = Bubble(size: size, color: color)
+        
+        if let node = bubble.component(ofType: NodeComponent.self)?.node {
+            node.position = position
+        }
+        
+        return bubble
+    }
+    
+    private func loadBubbles() {
+        let size = CGSize(width: frame.width / 4, height: frame.width / 4)
+        
+        let colors = [
+            SKColor(red: 0.83, green: 0.18, blue: 0.18, alpha: 1.00),
+            SKColor(red: 0.32, green: 0.18, blue: 0.66, alpha: 1.00),
+            SKColor(red: 0.10, green: 0.46, blue: 0.82, alpha: 1.00),
+            SKColor(red: 0.00, green: 0.59, blue: 0.65, alpha: 1.00),
+            SKColor(red: 0.22, green: 0.56, blue: 0.24, alpha: 1.00),
+            SKColor(red: 0.69, green: 0.71, blue: 0.17, alpha: 1.00),
+            SKColor(red: 0.96, green: 0.49, blue: 0.00, alpha: 1.00),
+            SKColor(red: 0.27, green: 0.35, blue: 0.39, alpha: 1.00)
+        ]
+        let positions = [
+            CGPoint(x: frame.midX - size.width / 2, y: frame.maxY * 2),
+            CGPoint(x: frame.midX - size.width / 2 - size.width, y: frame.maxY * 2),
+            CGPoint(x: frame.midX + size.width / 2, y: frame.maxY * 2),
+            CGPoint(x: frame.midX + size.width / 2 + size.width, y: frame.maxY * 2),
+            CGPoint(x: frame.midX - size.width / 2, y: frame.maxY * 2),
+            CGPoint(x: frame.midX - size.width / 2 - size.width, y: frame.maxY * 2),
+            CGPoint(x: frame.midX + size.width / 2, y: frame.maxY * 2),
+            CGPoint(x: frame.midX + size.width / 2 + size.width, y: frame.maxY * 2)
+        ]
+        
+        for i in 0..<8 {
+            bubbles.append(loadBubble(with: colors[i], size: size, at: positions[i]))
+        }
+    }
+    
+    private func spawnBubble() {
+        let bubbleNumber = randomizeBubble()
+        let bubble = bubbles[bubbleNumber].copy() as! Bubble
+        
+        addEntity(bubble)
+    }
+    
+    private func spawnBubbles() {
+        let wait = SKAction.wait(forDuration: 1, withRange: 1)
+        let spawn = SKAction.run { self.spawnBubble() }
+        let sequence = SKAction.sequence([wait, spawn])
+        
+        self.run(.repeatForever(sequence))
+    }
+    
+    private func randomizeBubble() -> Int {
+        let randomSource = GKMersenneTwisterRandomSource()
+        let bubbleNumber = randomSource.nextInt(upperBound: bubbles.count)
+        
+        return bubbleNumber
     }
     
 }
