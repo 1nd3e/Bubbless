4ae82dbd812ea34208a593ebe893ad2e77f9588f
1nd3e <vlad@hakkie.ru>
2020-05-03 20:48:51 +0300

    Создал сцену для получения дополнительной жизни

---
+++ Bubbless/Scenes/ExtraScene.swift
@@ -0,0 +1,14 @@
+//
+//  ExtraScene.swift
+//  Bubbless
+//
+//  Created by Vladislav Kulikov on 03.05.2020.
+//  Copyright © 2020 Vladislav Kulikov. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class ExtraScene: SKScene {
+    
+}
