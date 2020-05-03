e41148358be15bd221624562eb020d5060b08a12
1nd3e <vlad@hakkie.ru>
2020-05-03 20:52:03 +0300

    Добавил сцене физические тела

--- Bubbless/Scenes/ExtraScene.swift
+++ Bubbless/Scenes/ExtraScene.swift
@@ -15,6 +15,35 @@ class ExtraScene: SKScene {
     
 }
 
+// MARK: - Scene Events
+
+extension ExtraScene {
+    
+    override func didMove(to view: SKView) {
+        // Настраиваем параметры сцены игры
+        addPhysicsEdges()
+    }
+    
+}
+
+// MARK: - Scene Configuration
+
+extension ExtraScene {
+    
+    private func addPhysicsEdges() {
+        let bodyLT = SKPhysicsBody(edgeFrom: CGPoint(x: frame.minX, y: frame.maxY), to: CGPoint(x: frame.minX, y: frame.minY))
+        let bodyRT = SKPhysicsBody(edgeFrom: CGPoint(x: frame.maxX, y: frame.maxY), to: CGPoint(x: frame.maxX, y: frame.minY))
+        let bodyBT = SKPhysicsBody(edgeFrom: CGPoint(x: frame.minX, y: frame.minY), to: CGPoint(x: frame.maxX, y: frame.minY))
+        
+        let physicsBody = SKPhysicsBody(bodies: [bodyLT, bodyRT, bodyBT])
+        physicsBody.isDynamic = false
+        physicsBody.affectedByGravity = false
+        
+        self.physicsBody = physicsBody
+    }
+    
+}
+
 // MARK: - Entity Methods
 
 extension ExtraScene {
