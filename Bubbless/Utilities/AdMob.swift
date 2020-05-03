ecfb0dbfb631db798d67cf4c353266565732bfa5
1nd3e <vlad@hakkie.ru>
2020-05-04 02:53:47 +0300

    Написал логику AdMob

--- Bubbless/Utilities/AdMob.swift
+++ Bubbless/Utilities/AdMob.swift
@@ -6,6 +6,68 @@
 //  Copyright © 2020 Vladislav Kulikov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import GoogleMobileAds
 
-class AdMob {}
+protocol AdMobDelegate {
+    func userDidEarnReward()
+}
+
+class AdMob: NSObject {
+    
+    // MARK: - Types
+    
+    static let shared = AdMob()
+    
+    // MARK: - Properties
+    
+    var delegate: AdMobDelegate?
+    var viewController: UIViewController?
+    
+    private var rewardedAd: GADRewardedAd?
+    
+    private var userDidEarnReward = false
+    
+    // MARK: - Methods
+    
+    func start() {
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+    }
+    
+    func loadRewardedAd() {
+        rewardedAd = GADRewardedAd(adUnitID: "ca-app-pub-3940256099942544/1712485313")
+        
+        rewardedAd?.load(GADRequest()) { error in
+            if let error = error {
+                print("Unable to load ad: \(error.localizedDescription).")
+            }
+        }
+    }
+    
+    func showRewardedAd() {
+        if rewardedAd?.isReady == true {
+            if let viewController = viewController {
+                rewardedAd?.present(fromRootViewController: viewController, delegate: self)
+            }
+        }
+    }
+    
+}
+
+// MARK: - GADRewardedAdDelegate
+
+extension AdMob: GADRewardedAdDelegate {
+    
+    func rewardedAd(_ rewardedAd: GADRewardedAd, userDidEarn reward: GADAdReward) {
+        userDidEarnReward = true
+    }
+    
+    func rewardedAdDidDismiss(_ rewardedAd: GADRewardedAd) {
+        if userDidEarnReward {
+            delegate?.userDidEarnReward()
+        }
+        
+        loadRewardedAd()
+    }
+    
+}
