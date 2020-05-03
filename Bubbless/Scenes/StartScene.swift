71d84704e23d6d4ebb830229dd5477de3d3938f1
1nd3e <vlad@hakkie.ru>
2020-05-03 22:50:59 +0300

    Создал стартовую сцену

---
+++ Bubbless/Scenes/StartScene.swift
@@ -0,0 +1,38 @@
+//
+//  StartScene.swift
+//  Bubbless
+//
+//  Created by Vladislav Kulikov on 03.05.2020.
+//  Copyright © 2020 Vladislav Kulikov. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class StartScene: SKScene {
+    
+    private var entities = Set<GKEntity>()
+    
+}
+
+// MARK: - Entity Methods
+
+extension StartScene {
+    
+    private func addEntity(_ entity: GKEntity) {
+        entities.insert(entity)
+        
+        if let node = entity.component(ofType: NodeComponent.self)?.node {
+            addChild(node)
+        }
+    }
+    
+    private func removeEntity(_ entity: GKEntity) {
+        if let node = entity.component(ofType: NodeComponent.self)?.node {
+            node.removeFromParent()
+        }
+        
+        entities.remove(entity)
+    }
+    
+}
